21932e8d9c7eddedd6541c3b14557927bb829bdf
刘学 <1725948799@qq.com>
2019-01-04 09:37:09 +0800

    提交

--- js/NewProjects/task_designing.js
+++ js/NewProjects/task_designing.js
@@ -16,4 +16,22 @@ $(function(){
 	$('#cycle').click(function(){
 		$('#modal_cycle').show();
 	})
+	// 结束时间
+	$.selectDate("#endTime", {}, function(data) {
+		//      alert(data.year+"-"+data.month+"-"+data.day);
+		// console.log(data);
+		sStorage = window.localStorage; //本地存题目
+		sStorage.job = endTime.innerText;
+	});
+	var job = localStorage.getItem('job');
+	$('#endTime .desi_text').html(job);
+	$('.cancel img').click(function(){
+		var test1 = document.getElementById('test1');
+        if(test1.src.indexOf('registernoc') >= 0){
+            test1.src="../../image/mine/registerc.png";
+        }else{
+            test1.src="../../image/mine/registernoc.png";
+        }
+		
+	})
 })
